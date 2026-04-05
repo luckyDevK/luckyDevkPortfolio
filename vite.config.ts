7759ab5af1584bf79c0ab04f119913c2d0c7b3bb
very.lucky.kurniawan <very.lucky.kurniawan@mdd.co.id>
2026-04-05 15:02:42 +0700

    feat(vite.config.ts): add correct base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "luckyDevk-portfolio-new",
+  base: "/luckyDevkPortfolio/",
   build: {
     assetsInlineLimit: 0,
   },
